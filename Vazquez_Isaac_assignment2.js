1843734ca1ec432dd4928c1d91b931671695ac42
ivazquez1 <ivazquez1@fullsail.edu>
2015-01-15 21:00:29 -0600

    assignment2
    Vazquez isaac files

---
+++ Vazquez_Isaac_assignment2.js
@@ -0,0 +1,56 @@
+// Isaac Vazquez
+// project2 assignment
+// 01-15-2015
+
+var concString = "my name is" + " Isaac Vazquez.";
+
+console.log(concString);
+
+// My variables
+var proteinShake = "protein shake";
+var getTheBlenderToMix = "blender to mix";
+var proteinSkakeMix = " protein shake mix";
+var blender = "blender";
+
+// boolean function
+var putMilkInBlender = true;
+var putWaterInBlender = false;
+var usingBlueberries = true;
+var addingpeanutButter = true;
+
+
+// number function return value (4) scoops
+var getscoops = function (numscoops, name, message) {
+	var scoops = numscoops * numscoops;
+var numscoops = 2;
+var numscoops = 4;
+	
+	
+// string function
+var proteinShake = "muscle Mass proteinShake";	
+	
+
+// my first outputs
+console.log("the process of making a " + proteinShake + ".");
+console.log("get the blender and mix " + proteinShake  + ".");
+console.log("add milk to " + proteinShake + ".");
+console.log("or add water in " + proteinShake + ".");
+console.log("using blueberries to mix in " + proteinShake + ".");
+console.log("add peanutButter to mix with " + proteinShake + ".");
+console.log("add either two scoop in " + proteinShake + ".");
+console.log("or add four scoops instead of one into " + proteinShake + ".");
+console.log(name + scoops + " scoops " + message);
+};
+
+getscoops(2, "Isaac added ", "in the muscle mass protein shake");
+	
+console.log("now you can drink " + proteinShake + ".");
+
+
+// conditionals
+if (putMilkInBlender == true)
+if (putWaterInBlender == false)
+if (usingBlueberries == true)
+if (addingpeanutButter == true) {
+	// this is the code thats runs if its true;
+	}
